0b1e1b67abee15f4e2c16713b9b6ca6030c33555
= <=>
2018-09-13 00:41:14 +0300

    django framework

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/domantas.varapnickas/development/git/python/rest/python-rest-learning/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
